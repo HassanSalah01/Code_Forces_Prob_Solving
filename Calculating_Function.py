ff2c3c89b913dab3eba2b42b9fd8f738b6765902
HassanSalah01 <hassan.salah.eldine1@gmail.com>
2022-06-15 17:50:04 +0200

    Prob-24-486A

--- Calculating_Function.py
+++ Calculating_Function.py
@@ -10,26 +10,3 @@ def main():
         print(int(((x*(x+1))/2)/x))
 
 main()
-# def main():
-#     x = int(input())
-#     if(IsEven(x)):
-#         od =(x+1)/2
-#         od= od**2
-#         evn = (x-1)/2
-#         evn = 2*(evn*(evn+1))/2
-#     else:
-#         od = x/2
-#         od= od**2
-#         evn = x/2
-#         evn = 2*(evn*(evn+1))/2
-#     print((od*-1)+evn)
-
-# main()
-# 
-# 1000000001
-# x = 11
-# evn = (x-1)/2
-# evn = 2*(evn*(evn+1))/2
-# print(evn)
-
-print(int(((3052460231*(3052460231+1))//2)//3052460231))
