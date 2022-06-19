71cd64d9edcfd88601ed374172d370c8f28c02da
HassanSalah01 <hassan.salah.eldine1@gmail.com>
2022-06-19 23:10:12 +0200

    Prob-29-1328A

---
+++ Divisibility_Problem.py
@@ -0,0 +1,21 @@
+# def div(x,y):
+#     count = 0
+#     while(x%y!=0):
+#         count+=1
+#         x+=1
+#     return count
+
+def div(x,y):
+    count = 0 
+    for i in range(1,9):
+        if(x%(y*i)==0 and y*i>x):
+            print(i)
+            count = (y*i)-x
+            break
+        else:
+            x+=1
+        print(x)
+    return count
+
+print(div(10,4))
+
