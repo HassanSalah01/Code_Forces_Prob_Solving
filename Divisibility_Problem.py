9cf1b63b15a0e5c52cc62c3a2c60cdaac4e11610
HassanSalah01 <hassan.salah.eldine1@gmail.com>
2022-06-25 15:48:39 +0200

    Prob-32-1328A

--- Divisibility_Problem.py
+++ Divisibility_Problem.py
@@ -1,23 +1,17 @@
-x = 123
-y = 456
-
-
 def div(x,y):
-    f = x
-    s = y
-    t = 0
-    q=2 
-    while(x%y!=0 and q < y ):
-        t = y*q
-        q+=1
-        if(t%y==0 and t > f):
-            return t-x
-    return 0
-
-print(div)
+    if(x%y!=0):
+        return y-(x%y)
+    else:
+        return 0
 def main():
     x =int(input())
-    while(x>=0):
+    arr = []
+    while(x>0):
         inp = input()
         s = inp.split(" ")
-        print(div(int(s[0]),int(s[1])))
+        arr.append(div(int(s[0]),int(s[1])))
+        x-=1
+    for i in arr:
+        print(i)
+    
+main()
