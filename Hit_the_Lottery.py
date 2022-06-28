dd5a4836be153780fb102ab658eb419e8211dc05
HassanSalah01 <hassan.salah.eldine1@gmail.com>
2022-06-28 12:08:12 +0200

    Prob-36-996A

--- Hit_the_Lottery.py
+++ Hit_the_Lottery.py
@@ -1,34 +1,12 @@
-x = 1000000000
 def main():
     x = int(input())
     arr = [100,20,10,5,1]
     count = 0
     for i in arr:
-        while(x>=i):
-            x-=i
-            count+=1
-    print(count)
+        if(x>=i and x!=0):
+            count+=(x-(x%i))/i
+            x=x%i
+        else:
+            continue
+    print(int(count))
 main()
-
-
-
-
-# x = 125
-# arr = [100,20,10,5,1]
-# t = 0 
-# s = arr[t]
-# count = 0 
-# while(x and len(arr)>t):
-#     print(x,t,s,count)
-#     if(x%s<s):
-#         if(x>s):
-#             print("s")
-#             x = x%s
-#             t+=1
-#             s = arr[t]
-#             count+=1
-#     else:
-#         t+=1
-#         s = arr[t]
-
-# print(count)
