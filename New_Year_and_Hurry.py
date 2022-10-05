8384786d801e306e385501a57d1a8dbe1b2be5a1
HassanSalah01 <hassan.salah.eldine1@gmail.com>
2022-10-05 20:26:30 +0200

    Prob-43-750A

--- New_Year_and_Hurry.py
+++ New_Year_and_Hurry.py
@@ -1,8 +1,39 @@
 def main():
-    minutes = 4*60
     inp = input().split(" ")
+    time = (4*60)-int(inp[1])
+    result = 0 
+    prob = 0
+    x = 0
+    all = int(inp[0])
+    while(result < time):
+            x+=1
+            result = result+(5*x)
+            prob+=1
+    if(prob>all):
+        print(all)
+        return all
+    else:
+        print(prob)
+        return prob
+    
     
 
-
-
 main()
+# int time = (4*60)-190;
+#        int prob = 0 ;
+#        int result = 0;
+#        int x=0;
+#        int all = 4;
+#        while(result < time){
+#             x++;
+#             result = result+(5*x);
+#             prob++;
+#        }
+#        System.out.println(prob);
+#        if(prob>all){
+#         System.out.println(all);
+#        }else{
+#         System.out.println(prob);
+#        }
+
+#     }
