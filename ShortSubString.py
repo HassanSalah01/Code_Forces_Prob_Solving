8afc8e28706610b0f9e0e009cc3a772f29af36bc
HassanSalah01 <hassan.salah.eldine1@gmail.com>
2022-11-01 21:25:07 +0200

    Prob-51-1367A

---
+++ ShortSubString.py
@@ -0,0 +1,14 @@
+x = "abbaac"
+arr = []
+top = -1
+newstr=""
+print(len(arr))
+for i in x:
+    if(len(arr)==0):
+        arr.append[i]
+        top+=1
+    else:
+        if(i==arr[0]):
+            
+
+
